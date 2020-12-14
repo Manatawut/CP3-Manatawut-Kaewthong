5f62608c422c96d47609e19bd6c9c5066232e3d4
Manatawut <manatawut@gmail.com>
2020-12-14 13:38:15 +0700

    Create Exercise4_Manatawut_K.py

---
+++ Assignment/Exercise4_Manatawut_K.py
@@ -0,0 +1,15 @@
+FE = 75.5
+GB = 80.5
+ICS = 90.5
+CP = 78.5
+'''
+FE = Foundation English
+GB = General Business
+ICS = Introduction to Computer Systems
+CP = Computer Programming
+'''
+print("---Your Score---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
