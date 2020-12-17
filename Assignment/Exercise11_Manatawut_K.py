412c08f1d0f0a2acbc04657c03fce60041cdd14a
Manatawut <manatawut@gmail.com>
2020-12-17 21:10:47 +0700

    Update Exercise11_Manatawut_K.py

--- Assignment/Exercise11_Manatawut_K.py
+++ Assignment/Exercise11_Manatawut_K.py
@@ -2,4 +2,5 @@ inputHeight = int(input("The Height of the Pyramid : "))
 for i in range(inputHeight):
     space = " "*(inputHeight-i-1)
     star = "*"*(2*i+1)
-    print(space,star)
+    print(space,end="")
+    print(star)
